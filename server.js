d58fcfaa33edb6a1fed5ee54a9d1422611d0a279
Papsy-ops <papetua.narina@student.moringaschool.com>
2025-04-07 23:32:05 +0300

    WOE-3

--- server.js
+++ server.js
@@ -11,7 +11,6 @@ let image = require('./routes/image');
 // Initializing the app
 const app = express();
 
-
 // connecting the database
 
 //const MONGODB_URI = process.env.MONGODB_URI || config.mongoURI[app.settings.env]
@@ -31,21 +30,7 @@ mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true 
 // })
 
 
-// Connecting to the database using async/await
-const MONGODB_URI = process.env.MONGODB_URI || "mongodb+srv://papetuanarina:FMySwBDqf2O93rar@ip1.90y7ear.mongodb.net/darkroom-dev?retryWrites=true&w=majority";
 
-// Define an async function to connect to MongoDB
-async function connectToDatabase() {
-  try {
-    await mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
-    console.log(`Connected to Database: ${MONGODB_URI}`);
-  } catch (err) {
-    console.error('Error connecting to MongoDB:', err);
-  }
-}
-
-// Call the function to connect to the database
-connectToDatabase();
 
 // View Engine
 app.set('view engine', 'ejs');
@@ -53,25 +38,20 @@ app.set('view engine', 'ejs');
 // Set up the public folder;
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Body parser middleware
-app.use(express.json());
+// body parser middleware
+app.use(express.json())
+
 
 app.use('/', index);
 app.use('/image', image);
 
-// Start the server
-const PORT = process.env.PORT || 5000;
-<<<<<<< HEAD
-app.listen(PORT, '0.0.0.0' ,() => {
-  console.log(`Server is listening at http://localhost:${PORT}`);
-});
 
-module.exports = app;
-=======
+
+ 
+const PORT = process.env.PORT || 5000;
 app.listen(PORT,() =>{
     console.log(`Server is listening at http://localhost:${PORT}`)
 });
 
 
 module.exports = app;
->>>>>>> test
